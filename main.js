34fa0b0c3036fa02d251d1d76eab30b2dbbabe68
yelitza-rad <jelica_r@outlook.com>
2023-04-09 15:05:48 -0400

    add computed property for onSale

--- main.js
+++ main.js
@@ -7,7 +7,6 @@ const app = Vue.createApp({
       description:
         "Introducing our latest product - the Vue.js socks! These comfortable and stylish socks are perfect for any Vue.js enthusiast. Made from a soft and breathable material, these socks feature a colorful design inspired by the Vue.js framework. The design includes Vue.js logo and icons, making it a great gift for any web developer or tech enthusiast. These socks are perfect for wearing during coding sessions, attending meetups or conferences, or just lounging around the house. Available in a range of sizes, these Vue.js socks are a must-have for any fan of this powerful JavaScript framework. Order yours today and show off your love for Vue.js in style!",
       url: "https://vuejs.org/",
-      onSale: true,
       details: ["50% cotton", "30% wool", "20% polyester"],
       variants: [
         {
@@ -16,6 +15,7 @@ const app = Vue.createApp({
           size: ["s", "m", "l", "xl"],
           image: "./assets/images/socks_green.jpg",
           quantity: 50,
+          onSale: true,
         },
         {
           id: 2,
@@ -23,6 +23,7 @@ const app = Vue.createApp({
           size: ["xs", "s", "m"],
           image: "./assets/images/socks_blue.jpg",
           quantity: 0,
+          onSale: false,
         },
       ],
       selectedVariantId: null,
@@ -51,6 +52,9 @@ const app = Vue.createApp({
     },
     inStock() {
       return this.variants[this.selectedVariant].quantity;
+    },
+    onSale() {
+      return this.variants[this.selectedVariant].onSale;
     },
   },
 });
